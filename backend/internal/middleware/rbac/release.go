ae32c86f7969e48d4c4f7c940e30f5d927c263d5
Vadim Averinov <gwert2601@gmail.com>
2026-05-06 22:07:00 +0300

    [BE-29] Реализовать RBAC guard для deploy/rollback

---
+++ backend/internal/middleware/rbac/release.go
@@ -0,0 +1,12 @@
+package rbac
+
+import (
+	"net/http"
+
+	rbaccore "github.com/mams/backend/internal/rbac"
+)
+
+func RequireReleaseManageAccess(services serviceReader, access accessReader, next http.Handler) http.Handler {
+	return RequireAtLeastRole(rbaccore.RoleDeveloper, services, access, next)
+}
+
